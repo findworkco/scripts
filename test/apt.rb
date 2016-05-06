662d1a7ce6d438d0f809c80fa388ba2889eb53b8
Todd Wolfson <todd@twolfson.com>
2016-05-06 16:09:57 -0500

    Fixed up tests as well

--- test/apt.rb
+++ test/apt.rb
@@ -5,7 +5,8 @@ require_relative "utils/serverspec"
 describe "apt" do
   it "was updated within past 24 hours" do
     one_day_ago = DateTime.now() - 60 * 60 * 24
-    timestamp_file = file("/var/cache/apt/pkgcache.bin")
+    timestamp_filepath = ENV["CI"] ? "/var/lib/apt/periodic/update-success-stamp" : "/var/cache/apt/pkgcache.bin"
+    timestamp_file = file(timestamp_filepath)
     expect(timestamp_file.mtime).to(be >= one_day_ago)
   end
 end

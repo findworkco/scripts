f3252cf319df0d06e8533a21391ff39cf21778b9
Todd Wolfson <todd@twolfson.com>
2016-05-06 18:34:38 -0500

    Fixed up lint error

--- src/cookbooks/common/recipes/default.rb
+++ src/cookbooks/common/recipes/default.rb
@@ -139,8 +139,8 @@ service "ssh" do
   # Always enable and run our SSH server
   # https://docs.chef.io/resource_service.html#examples
   # DEV: Wercker seems to use Init whereas Vagrant uses Upstart
-  # DEV: We use `Dir.exists?` over `ENV` since we restrict our variable passing in `findwork.co.sh`
-  provider(Dir.exists?("/pipeline") ? Chef::Provider::Service::Init : Chef::Provider::Service::Upstart)
+  # DEV: We use `Dir.exist?` over `ENV` since we restrict our variable passing in `findwork.co.sh`
+  provider(Dir.exist?("/pipeline") ? Chef::Provider::Service::Init : Chef::Provider::Service::Upstart)
   supports(:reload => true, :restart => true, :status => true)
   action([:start])
 end

355262d7ee347673d95de81912a3be9670f06d71
Todd Wolfson <todd@twolfson.com>
2016-05-06 17:36:16 -0500

    Fixed up scripts

--- bin/bootstrap-vagrant.sh
+++ bin/bootstrap-vagrant.sh
@@ -58,9 +58,12 @@ src_dir="/vagrant/src"
 . bin/_bootstrap.sh
 
 # Install development scripts and repos
+if ! which git &> /dev/null; then
+  sudo apt-get install -y git
+fi
 if ! test -f ~/quick-start-app.sh; then
   ln -s "$base_dir/bin/quick-start-app.sh" ~/quick-start-app.sh
 fi
-if ! test -f "$base_dir/app"; then
+if ! test -d "$base_dir/app"; then
   git clone git@github.com:twolfson/find-work-app.git "$base_dir/app"
 fi
--- bin/quick-start-app.sh
+++ bin/quick-start-app.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 # DEV: We skip `set -e` as we are executing this via `.`
+# Navigate to the repo directory
+cd /vagrant/app
+
+# Run its start script
+ENV=development ./start.sh

a151aafb70902d9bcb632727a371e35cef0818c1
Todd Wolfson <todd@twolfson.com>
2016-05-06 16:04:23 -0500

    Removed last commit integration

--- bin/test-wercker.sh
+++ bin/test-wercker.sh
@@ -6,4 +6,7 @@ set -e
 export SERVERSPEC_BACKEND="exec"
 
 # Run our tests
+# TODO: Add tests for diff-based build
+#   This is pending on multiple environment support in Wercker
+#   https://github.com/wercker/support/issues/26
 . bin/_test.sh

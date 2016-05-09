dfb902633c5f0926772f5655a4ca29be782ba7ad
Todd Wolfson <todd@twolfson.com>
2016-05-09 13:23:19 -0500

    Added lint files (#8)
    Our linter is failing locally due to linting `app`. In this PR:
    
    - Moved to explicit files list for linting

--- bin/_test.sh
+++ bin/_test.sh
@@ -3,8 +3,9 @@
 set -e
 
 # If we should lint our files, then run our linter
+# DEV: We manually specify files to skip linting `app/`
 if test "$SKIP_LINT" != "TRUE"; then
-  bin/rubocop
+  bin/rubocop Gemfile Vagrantfile bin/ data/ docs/ src/ test/
 fi
 
 # Run our rspec tests (depends on SSH_CONFIG, TARGET_HOST)

d3fb2a9f45b5c3fc8d370c5dab18e58fefa4139f
Todd Wolfson <todd@twolfson.com>
2016-09-12 14:29:03 -0700

    Added our command

--- src/cookbooks/findwork.co/recipes/postgresql-user-exists.sh
+++ src/cookbooks/findwork.co/recipes/postgresql-user-exists.sh
@@ -2,3 +2,7 @@
 # Exit on first error
 set -e
 
+# Prepare and run our command
+find_work_query_command="psql postgres --command \"SELECT usename FROM pg_user WHERE usename='find-work';\" --tuples --no-align"
+test "$(sudo su postgres --shell /bin/bash --command "$find_work_query_command")" = "\n"
+

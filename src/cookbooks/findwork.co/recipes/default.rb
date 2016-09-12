7f4a30eb110d9461f79f3c8b788584cb1334f16d
Todd Wolfson <todd@twolfson.com>
2016-09-12 14:24:25 -0700

    Started on our script

--- src/cookbooks/findwork.co/recipes/default.rb
+++ src/cookbooks/findwork.co/recipes/default.rb
@@ -1,14 +1,6 @@
 # Load in our dependencies
 include_recipe "common"
 
-# Define common helpers
-def sops_get(key)
-  data_dir = ENV.fetch("data_dir")
-  sops_secret_filepath = "#{data_dir}/var/sops/find-work/scripts/secret.yml"
-  # TODO: Escape for shell execution
-  puts `sops #{sops_secret_filepath} --decrypt --extract '#{key}'`
-end
-
 # Guarantee `node` is installed
 # @depends_on execute[apt-get-update-periodic]
 # https://github.com/nodesource/distributions/tree/96e9b7d40b6aff7ade7bc130d9e18fd140e9f4f8#installation-instructions
@@ -87,6 +79,24 @@ bash "add-postgresql-find-work" do
 
   # Then create our user
   code <<-EOF
-    echo 'hello world'
+    # Exit on first error and don't allow unset variables
+    set -e
+    set -u
+
+    # Fetch our user's password
+    password="find-work"
+    if test "$use_sops" = "TRUE"; then
+      sops_secret_filepath = "$data_dir/var/sops/find-work/scripts/secret.yml"
+      key="["find_work_db_user_password"]"
+      password="$(sops "$sops_secret_filepath" --decrypt --extract "$key")"
+    fi
+
+    # Create our user
+    # create_user_command="psql --command \"CREATE ROLE vagrant WITH CREATEDB;\""
+    # sudo su postgres --shell /bin/bash --command "$create_user_command"
+
+    # Set our user's password
+    # set_user_password="psql --command \"ALTER ROLE vagrant WITH PASSWORD 'vagrant';\""
+    # sudo su postgres --shell /bin/bash --command "$set_user_password"
   EOF
 end

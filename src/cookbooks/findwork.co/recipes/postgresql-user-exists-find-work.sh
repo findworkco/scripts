e52cf399b63a49ff5fd5ee152088f63ad16c4ee3
Todd Wolfson <todd@twolfson.com>
2016-09-12 14:52:12 -0700

    Fixed up roles vs users

--- src/cookbooks/findwork.co/recipes/postgresql-user-exists-find-work.sh
+++ src/cookbooks/findwork.co/recipes/postgresql-user-exists-find-work.sh
@@ -4,6 +4,7 @@ set -e
 
 # Prepare and run our command
 user="find_work"
-find_work_query_command="psql postgres --command \"SELECT usename FROM pg_user WHERE usename='$user';\" --tuples --no-align"
-test "$(sudo su postgres --shell /bin/bash --command "$find_work_query_command")" != "\n"
+find_work_query_command="psql postgres --command \"SELECT rolname FROM pg_role WHERE rolname='$user';\" --tuples --no-align"
+# test "$(sudo su postgres --shell /bin/bash --command "$find_work_query_command")" != "\n"
+sudo su postgres --shell /bin/bash --command "$find_work_query_command"
 

5bc089fcd45ff25bcc040d77cb67561193359991
Todd Wolfson <todd@twolfson.com>
2016-09-12 17:11:30 -0700

    Completed SOPS step

--- src/cookbooks/findwork.co/recipes/postgresql-add-user-find-work.sh
+++ src/cookbooks/findwork.co/recipes/postgresql-add-user-find-work.sh
@@ -9,22 +9,23 @@ set -u
 # TODO: Need to verify out `only_if` logic stands (run the script in Vagrant)
 # TODO: Re-enable `Create our user` when we are good to go
 # TODO: Verify our password works
+# TODO: Add test that remote has secure password yet local has insecure password
 
 # Fetch our user's password
+# DEV: We cannot run this branch inside of Vagrant due to
 user="find_work"
 password="find_work"
 if test "$use_sops" = "TRUE"; then
   sops_secret_filepath="$data_dir/var/sops/find-work/scripts/secret.yml"
-  key='["find_work_db_user_password"]'
-  sops "$sops_secret_filepath" --decrypt
-  # password="$(sops "$sops_secret_filepath" --decrypt --extract '$key')"
+  key="[\"find_work_db_user_password\"]"
+  password="$(sops "$sops_secret_filepath" --decrypt --extract "$key")"
 fi
 echo "$password"
 
-# Create our user
+# # Create our user
 # create_user_command="psql --command \"CREATE ROLE $user WITH CREATEDB LOGIN;\""
 # sudo su postgres --shell /bin/bash --command "$create_user_command"
 
-# Set our user's password
-set_user_password="psql --command \"ALTER ROLE $user WITH PASSWORD '$password';\""
-sudo su postgres --shell /bin/bash --command "$set_user_password"
+# # Set our user's password
+# set_user_password="psql --command \"ALTER ROLE $user WITH PASSWORD '$password';\""
+# sudo su postgres --shell /bin/bash --command "$set_user_password"

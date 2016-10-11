adf7a226d34dbf9058f5cc305772788aa7a3eaab
Todd Wolfson <todd@twolfson.com>
2016-10-10 18:37:38 -0700

    Upgraded to latest Node.js version (#33)
    Another minor Node.js version has been released, time to upgrade to it. In this PR:
    
    - Upgraded from Node.js@4.5.0 to Node.js@4.6.0

--- test/node.rb
+++ test/node.rb
@@ -4,10 +4,10 @@ require_relative "utils/serverspec"
 # Start our tests
 describe "Node.js" do
   it "is installed via apt" do
-    expect(package("nginx")).to(be_installed())
+    expect(package("nodejs")).to(be_installed())
   end
 
   it "has our expected version" do
-    expect(command("node --version").stdout).to(eq("v4.5.0\n"))
+    expect(command("node --version").stdout).to(eq("v4.6.0\n"))
   end
 end

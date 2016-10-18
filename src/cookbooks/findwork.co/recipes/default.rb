3ddcf6d49d484ef439c2aeece4c9c32b33e500b1
Todd Wolfson <todd@twolfson.com>
2016-10-18 14:55:39 -0700

    Upgraded to Node.js@4.6.1

--- src/cookbooks/findwork.co/recipes/default.rb
+++ src/cookbooks/findwork.co/recipes/default.rb
@@ -11,8 +11,8 @@ execute "add-nodejs-apt-repository" do
   only_if("! which node")
 end
 apt_package "nodejs" do
-  version("4.6.0-1nodesource1~trusty1")
-  only_if("test \"$(node --version)\" != \"v4.6.0\"")
+  version("4.6.1-1nodesource1~trusty1")
+  only_if("test \"$(node --version)\" != \"v4.6.1\"")
 end
 
 # Guarantee `git` is installed (required for `bower`)
--- test/node.rb
+++ test/node.rb
@@ -8,6 +8,6 @@ describe "Node.js" do
   end
 
   it "has our expected version" do
-    expect(command("node --version").stdout).to(eq("v4.6.0\n"))
+    expect(command("node --version").stdout).to(eq("v4.6.1\n"))
   end
 end

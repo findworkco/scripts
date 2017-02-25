68a4ac283a8af9fc577e9c7c752e5ea4b9b1d8e8
Todd Wolfson <todd@twolfson.com>
2017-02-24 16:22:54 -0800

    Added our log directory for application

---
+++ test/fs.rb
@@ -0,0 +1,12 @@
+# Load in our dependencies
+require_relative "utils/serverspec"
+
+# Start our tests
+describe "Filesystem" do
+  it "has a folder for our application logs" do
+    log_app_dir = file("/var/log/findworkco/app")
+    expect(log_app_dir.mode).to(eq((USER_RWX | GROUP_RX | OTHER_RX).to_s(8)))
+    expect(log_app_dir.owner).to(eq(ROOT_USER))
+    expect(log_app_dir.group).to(eq(ROOT_GROUP))
+  end
+end

c39c94e7b57fe519ab062f9e966ffe1056812792
Todd Wolfson <todd@twolfson.com>
2017-02-27 20:32:10 -0800

    Removed htpasswd from NGINX

--- src/cookbooks/findwork.co/recipes/default.rb
+++ src/cookbooks/findwork.co/recipes/default.rb
@@ -21,9 +21,7 @@ apt_package("git")
 # Configure NGINX for `findwork.co` node
 # @depends_on service[nginx]
 data_file "/etc/nginx/conf.d/findwork.co.htpasswd" do
-  owner("root")
-  group("root")
-  mode("644") # u=rw,g=r,o=r
+  action(:delete)
 end
 data_file "/etc/nginx/conf.d/findwork.co.conf" do
   owner("root")

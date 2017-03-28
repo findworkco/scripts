096d2771063fde117e22de75cd9d5f0faf9ccd56
Todd Wolfson <todd@twolfson.com>
2017-03-27 19:44:28 -0700

    Fixing up lint errors

--- src/config.rb
+++ src/config.rb
@@ -4,7 +4,7 @@ require "yaml"
 # Resolve and export our local config
 # http://stackoverflow.com/a/86325
 filepath = "/var/find-work/scripts/index.yml"
-if not File.exists?(filepath)
+if !File.exist?(filepath)
   abort("#{filepath} doesn't exist. " +
     "Please run `bin/install-config-{{type}}.sh` before running `bootstrap.sh`")
 end
